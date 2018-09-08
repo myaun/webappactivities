709363d99f6c541197addb46b9e9ba1d4ae3f507
myaun <41812728+myaun@users.noreply.github.com>
2018-09-08 14:41:50 +0800

    Update view.php

--- workshop05/view.php
+++ workshop05/view.php
@@ -53,40 +53,7 @@ if ( isset($_POST['make']) && isset($_POST['year'])
         ?>
         <h1><p>Tracking Autos for </p></h1>
         <?php echo "<h1>" . $_SESSION['email'] . "</h1>";?>
-
-        <form method="post">
-            <?php echo "<font color=green>".$msg1."</font>"; ?> 
-            <p>Make:
-                <input type="text" name="make"</p>
-            <p>Year:
-                <input type="text" name="year"></p>
-            <p>Mileage:
-                <input type="text" name="mileage"></p>
-            <p>
-                <input type="submit" name="action" value="Add"/>  
-                <input type="submit" name="action" value="Logout"/>
-            </p>   
-            </p><?php echo "<h2> Automobiles </h2>"; ?></p>
-
-            <?php 
-                $stmt = $pdo->query("SELECT * FROM autos");
-                echo '
-                        <table border="1">
-                        <tr>
-                            <th>MAKE</th>
-                            <th>YEAR</th>
-                            <th>MILEAGE</th>
-                        </tr>
-                    ';
-                while ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
-                    echo '<tr>'.
-                            '<td>'.$row['make'].'</td>'.
-                            '<td>'.$row['year'].'</td>'.
-                            '<td>'.$row['mileage'].'</td>'.
-                         '</tr>';
-                }
-                echo '</table>';
-            ?>
-        </form>
+	<p><a href="add.php">Add New</a> | <a href="index.php">Logout</a></p>
     </body>
 </html>
+
